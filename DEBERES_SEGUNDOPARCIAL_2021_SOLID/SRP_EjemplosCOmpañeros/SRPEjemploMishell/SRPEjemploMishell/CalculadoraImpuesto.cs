a98fd171142b93fdb55c1ae6ed6890e08acca3c0
Anderson Chanchay <andersonchanchay@iMac-de-Anderson.local>
2021-01-08 18:01:20 -0500

    Agregando Proyectos Compañeros de Curso

---
+++ DEBERES_SEGUNDOPARCIAL_2021_SOLID/SRP_EjemplosCOmpañeros/SRPEjemploMishell/SRPEjemploMishell/CalculadoraImpuesto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SRPEjemploMishell
+{
+    class CalculadoraImpuesto
+    {
+        public double CalcularImpuestoIessDelTrabajador(double sueldo)
+        {
+            return sueldo * 9.45 / 100;
+        }
+        public double CalcularImpuestoIessDelPatrono(double sueldo)
+        {
+            return sueldo * 12.25 / 100;
+        }
+    }
+}
---
+++ DEBERES_SEGUNDOPARCIAL_2021_SOLID/SRP_EjemplosCOmpañeros/SRPEjemploMishell/SRPEjemploMishell/Empleado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SRPEjemploMishell
+{
+    public class Empleado
+    {
+
+        public string Nombre { get; set; }
+        public string Puesto { get; set; }
+        public int Edad { get; set; }
+        public double Sueldo { get; set; }
+
+    }
+}
