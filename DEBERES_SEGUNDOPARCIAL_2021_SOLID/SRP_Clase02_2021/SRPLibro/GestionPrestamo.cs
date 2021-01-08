1cdbdc6d15e6beffd72cd4343e348b71ed07d90e
Anderson Chanchay <andersonchanchay@iMac-de-Anderson.local>
2021-01-08 15:16:02 -0500

    añadiendo codigo 2021 principios solid

---
+++ DEBERES_SEGUNDOPARCIAL_2021_SOLID/SRP_Clase02_2021/SRPLibro/GestionPrestamo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SRPLibro
+{
+    public class GestionPrestamo
+    {
+        public GestionPrestamo()
+        {
+            
+        }
+
+        public void ArrendarLibro(int id, int idPersona)
+        {
+            id = idPersona;
+        }
+
+        public void DevolverLibro(int idLibro)
+        {
+            idLibro = idLibro;
+        }
+    }
+}
---
+++ DEBERES_SEGUNDOPARCIAL_2021_SOLID/SRP_Clase02_2021/SRPLibro/Texto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SRPLibro
+{
+    internal class Texto
+    {
+
+        internal static void GuardarTexto(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void EliminarTexto(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
